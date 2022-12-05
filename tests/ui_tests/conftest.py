78be0d0a852a1c71829a5ac5b14e4fc5127e810d
Nestandarta <zona2033@yandex.ru>
2022-12-06 01:51:08 +0300

    new yaml

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -1,17 +1,15 @@
-import chromedriver_autoinstaller
 import pytest
 from selene.support.shared import browser
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(1920, 1024))
+display = Display(visible=0, size=(1200, 1200))
 display.start()
-chromedriver_autoinstaller.install()
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = f'https://demo.vinteo.com'
-    browser.config.window_width = 1920
+    browser.config.window_width = 1100
     browser.config.window_height = 1024
     yield
 
