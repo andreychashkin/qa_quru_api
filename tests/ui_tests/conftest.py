0a77b8bd4a450fb2e877c68398a6fa973846505b
Nestandarta <zona2033@yandex.ru>
2022-12-06 02:06:32 +0300

    new yaml

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -9,6 +9,7 @@ display.start()
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = f'https://demo.vinteo.com'
+    browser.config.browser_name = 'firefox'
     browser.config.window_width = 1100
     browser.config.window_height = 1024
     yield
