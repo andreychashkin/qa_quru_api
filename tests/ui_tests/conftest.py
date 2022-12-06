3cc97ed789c79ddcd08f7bf3c657f5942d8ca2f5
Nestandarta <zona2033@yandex.ru>
2022-12-06 11:48:21 +0300

    32 ui tests

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selene.support.shared import browser
 from pyvirtualdisplay import Display
+from api import *
 
 display = Display(visible=False, size=(1920, 1100))
 display.start()
@@ -9,7 +10,7 @@ display.start()
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = f'https://demo.vinteo.com'
-    # browser.config.browser_name = 'firefox'
+    browser.config.browser_name = TestData.selene_browser
     browser.config.window_width = 1920
     browser.config.window_height = 1024
     yield
