a7bcea2a8aa99dd3c261ecc30d4e54122e27bd58
Nestandarta <zona2033@yandex.ru>
2022-12-06 11:48:21 +0300

    32 ui tests

--- api/libs/TestData.py
+++ api/libs/TestData.py
@@ -17,7 +17,8 @@ if os.environ.get('SECOND_IP') is None:
     second_ip = '10.23.9.55'
 else:
     second_ip = os.environ.get('SECOND_IP')
-
+if os.environ.get('BROWSER') is None:
+    selene_browser = 'chrome'
 
 if os.environ.get('CALL_IP') is None:
     call_ip = '10.23.9.59:4567'
