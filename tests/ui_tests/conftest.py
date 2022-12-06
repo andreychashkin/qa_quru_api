354810021a39a1ab06038ea1e30ce03618300454
Nestandarta <zona2033@yandex.ru>
2022-12-06 10:17:21 +0300

    deleted tests

--- tests/ui_tests/conftest.py
+++ tests/ui_tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene.support.shared import browser
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(1200, 1200))
+display = Display(visible=0, size=(1920, 1100))
 display.start()
 
 
@@ -10,7 +10,7 @@ display.start()
 def browser_management():
     browser.config.base_url = f'https://demo.vinteo.com'
     browser.config.browser_name = 'firefox'
-    browser.config.window_width = 1100
+    browser.config.window_width = 1920
     browser.config.window_height = 1024
     yield
 
