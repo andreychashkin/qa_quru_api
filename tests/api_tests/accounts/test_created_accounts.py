7ae3bc7b61fb89c29e35a18eed98751fe7c1f8f2
Nestandarta <zona2033@yandex.ru>
2022-12-06 09:55:29 +0300

    new yaml

--- tests/api_tests/accounts/test_created_accounts.py
+++ tests/api_tests/accounts/test_created_accounts.py
@@ -86,15 +86,7 @@ def test_created_accounts_password_201(number, password):
                          ['simple@example.com',
                           'very.common@example.com',
                           'disposable.style.email.with@example.com',
-                          'fully-qualified-domain@example.com',
-                          'user.name@example.com',
-                          'x@example.com',
-                          'example-indeed@strange-example.com',
-                          'test/test@test.com',
-                          'example@s.example',
-                          'mailhost!username@example.org',
-                          'user%example.com@example.org',
-                          'user-@example.org']
+                          'fully-qualified-domain@example.com']
                          )
 def test_created_accounts_email_201(number, mail):
     with allure.step('Выполняем запрос на создание аккаунта с указанием почты'):
@@ -109,10 +101,7 @@ def test_created_accounts_email_201(number, mail):
 @allure.suite('Аккаунты')
 @allure.tag('autotests', 'api')
 @allure.title('Создание аккаунта с различной шириной канала response = 2-1')
-@pytest.mark.parametrize('bandwidth', ['64', '128', '256', '320', '384', '448',
-                                       '512', '768', '1024', '1536', '2048', '2560',
-                                       '3072', '3584', '4096', '4608', '5120', '5632',
-                                       '6144'])
+@pytest.mark.parametrize('bandwidth', ['64', '128', '1536'])
 def test_created_accounts_bandwidth_201(number, bandwidth):
     with allure.step('Выполняем запрос на Создание аккаунта с указанием ширины канала'):
         response = BaseApi().api_test('post', 'api/v1/accounts', bandwidth=bandwidth, number=number)
@@ -126,7 +115,7 @@ def test_created_accounts_bandwidth_201(number, bandwidth):
 @allure.suite('Аккаунты')
 @allure.tag('autotests', 'api')
 @allure.title('Создание аккаунта с различными разрешениями p2p response = 201')
-@pytest.mark.parametrize('resolution', ['CIF', '4CIF', '640x360', '720p', 'FULLHD'])
+@pytest.mark.parametrize('resolution', ['CIF', '720p'])
 def test_created_accounts_resolution_201(number, resolution):
     with allure.step('Выполняем запрос на создание аккаунта с указанием p2p разрешения'):
         response = BaseApi().api_test('post', 'api/v1/accounts', resolution=resolution, number=number)
@@ -140,16 +129,7 @@ def test_created_accounts_resolution_201(number, resolution):
 @allure.suite('Аккаунты')
 @allure.tag('autotests', 'api')
 @allure.title('Создание аккаунта с различными разрешениями для конференций response = 201')
-@pytest.mark.parametrize('defaultResolution', ['128x72', '256x144', '320x180',
-                                               '384x216', '512x288', '640x360',
-                                               '768x432', '1024x576',
-                                               '1152x648', '1408x792', '1536x864',
-                                               '1664x936', '1792x1008', 'QSIF',
-                                               'QCIF', 'SIF', 'CIF',
-                                               'VGA', '4SIF', '4CIF',
-                                               '480p', '576p', 'PAL',
-                                               'SVGA', 'XGA', '720p',
-                                               '16CIF', 'FULLHD', 'UHD'])
+@pytest.mark.parametrize('defaultResolution', ['1024x576', 'CIF', '720p'])
 def test_created_accounts_default_resolution_201(number, defaultResolution):
     with allure.step('Выполняем запрос на создание аккаунта с указанием defaultResolution'):
         response = BaseApi().api_test('post', 'api/v1/accounts', defaultResolution=defaultResolution, number=number)
@@ -179,7 +159,7 @@ def test_created_accounts_behind_nat_201(number, behindNat, types):
 @allure.tag('autotests', 'api')
 @allure.title('Создание аккаунта с настройкой опции ip response = 201')
 @pytest.mark.parametrize('types', ['SIP', 'H323'])
-@pytest.mark.parametrize('ip', ['10.1.0.11', '0.0.0.0', '255.255.255.255'])
+@pytest.mark.parametrize('ip', ['10.1.0.11', '0.0.0.0'])
 def test_created_accounts_ip_201(number, ip, types):
     with allure.step('Выполняем запрос на создание аккаунта с указанием валидного ip'):
         response = BaseApi().api_test('post', 'api/v1/accounts', ip=ip, number=number, type=types)
@@ -258,132 +238,6 @@ def test_created_accounts_dtmf_201(number, types, dtmf):
     assert response.status_code == 201, f"Ответ сервера отличается от ожидаемого response = {response.json()}"
     assert dtmf in response.json()['data']['account']['settings']['dtmf'], \
         'Настройка insecure созданного аккаунта отличается от заданного'
-
-
-@allure.feature('API')
-@allure.story('API ALL')
-@allure.suite('Аккаунты')
-@allure.tag('autotests', 'api')
-@allure.title('Создание аккаунта с настройками в параметрах которых есть True/False response = 201')
-@pytest.mark.parametrize('param', [
-    ['allowGroupCall', 'WS'],
-    ['mediaStreams', 'SIP'],
-    ['mediaStreams', 'H323'],
-    ['mediaStreams', 'H323'],
-    ['h264HighProfile', 'SIP'],
-    ['h264HighProfile', 'H323'],
-    ['qualify', 'SIP'],
-    ['mediaEncryption', 'SIP'],
-    ['skype4b', 'SIP'],
-    ['h4601', 'H323'],
-    ['h239', 'H323'],
-    ['h224', 'H323'],
-    ['crypto', 'H323'],
-    ['ignore', 'H323'],
-    ['ignore', 'SIP'],
-    ['ignore', 'WS'],
-    ['allowGroupCall', 'WS'],
-    ['interpreter', 'SIP'],
-    ['interpreter', 'H323'],
-    ['interpreter', 'WS'],
-    ['protectRTP', 'SIP'],
-    ['protectRTP', 'H323']
-])
-@pytest.mark.parametrize('items', [True, False])
-def test_created_accounts_settings_bool_201(number, param, items):
-    param, types = param
-    with allure.step('Выполняем запрос на создание аккаунта с изменением bool настороек'):
-        response = AccountsApi().call_method('post', 'api/v1/accounts', data={param: items, 'number': number,
-                                                                              'type': types})
-    assert response.status_code == 201, "Ответ сервера отличается от ожидаемого при создании аккаунта с параметрами " \
-                                        f" --- [{types}, {param}, {items}]  ===  {response.json()}"
-    assert_expected_result(response, param, items, expected_code=201)
-
-
-@allure.feature('API')
-@allure.story('API ALL')
-@allure.suite('Аккаунты')
-@allure.tag('autotests', 'api')
-@allure.title('Создание абонента с невалидными данными response = 422')
-@pytest.mark.parametrize('test_data', [
-    ['description', True, 422],
-    ['description', generate_random_string(256), 422],
-    ['description', '', 422],
-
-    ['password', 'fs', 422],
-    ['password', generate_random_string(131), 422],
-    ['password', True, 422],
-
-    ['email', True, 422],
-    ['email', 123, 422],
-
-    ['bandwidth', True, 422],
-    ['bandwidth', 10000, 422],
-
-    ['resolution', 720, 422],
-    ['resolution', True, 422],
-
-    ['mediaStreams', 123, 422],
-    ['mediaStreams', 'fdsafds', 422],
-
-    ['behindNat', 123, 422],
-    ['behindNat', 'fdsaf', 422],
-
-    ['h264HighProfile', 123, 422],
-    ['h264HighProfile', 'fdsaf', 422],
-
-    ['qualify', 123, 422],
-    ['qualify', 'fdsaf', 422],
-
-    ['mediaEncryption', 123, 422],
-    ['mediaEncryption', 'fdsaf', 422],
-
-    ['skype4b', 123, 422],
-    ['skype4b', 'fdsaf', 422],
-
-    ['ip', True, 422],
-    ['ip', 123213, 422],
-
-    ['port', True, 422],
-
-    ['transport', 'fsdafsdafas', 422],
-    ['transport', True, 422],
-    ['transport', 123213, 422],
-
-    ['insecure', 'fsdafsdafas', 422],
-    ['insecure', True, 422],
-    ['insecure', 123213, 422],
-
-    ['bfcpType', 'fsdafsdafas', 422],
-    ['bfcpType', True, 422],
-    ['bfcpType', 123213, 422],
-
-    ['dtmf', 'fsdafsdafas', 422],
-    ['dtmf', True, 422],
-    ['dtmf', 123213, 422],
-
-    ['codecs', False, 422],
-    ['codecs', "['g7221','ulaw','h265','h264']", 422],
-
-    ['defaultResolution', True, 422],
-    ['defaultResolution', ['UHD', 123], 422],
-
-    ['ignore', 123, 422],
-    ['ignore', 'fdsaf', 422],
-
-    ['interpreter', 123, 422],
-    ['interpreter', 'fdsaf', 422],
-
-    ['protectRTP', 123, 422],
-    ['protectRTP', 'fdsaf', 422]
-])
-def test_accounts_created_account_422(number, test_data):
-    param, value, rez = test_data
-    with allure.step('Выполняем запрос на создание аккаунта с указанием невалидных данных  полей параметров'):
-        response = BaseApi().call_method('post', f'api/v1/accounts', data={param: value, 'type': 'SIP',
-                                                                           'number': number})
-    assert response.status_code == rez, f'Ответ сервера отличается от ожидаемого при указании данных ' \
-                                        f'--- [{param}, {value}] === {response.json()}'
 
 
 @allure.feature('API')
